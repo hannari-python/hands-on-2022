1184c91064f941102eefd38f9a851de365f62b2e
hiroshi sano <23502+hrsano645@users.noreply.github.com>
2022-02-13 11:49:21 +0900

    add py file

---
+++ 202202-hands-on/sano-san/convert_api_to_excel.py
@@ -0,0 +1,17 @@
+# PythonでAPIにアクセスしてきて取得したデータをExcelファイルに書き出してみる
+
+import urllib
+import json
+from openpyxl import Workbook
+
+def get_api_data(api_url: str) -> dict:
+    pass
+
+def generate_excelfile_from_api_data(api_json_data: dict) -> None:
+    pass
+
+def main() -> None:
+    pass
+
+if __name__=="__main__":
+    main()
